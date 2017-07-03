14b60d9e29406466d4882e9bbd7e6f811281227d
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-07-03 23:35:20 +0300

    1st version

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+check_sites_health.py takes domain names from input file and 
+checks if the site is responding plus provide Domain Registry Expiry Date
 
+
+# Sample usage and output
+*python check_sites_health.py sites_url.txt*
+```
+site sokolovdp.info is alive, Registry Expiry Date: 2018-03-21T17:14:03Z
+site sokolovdp.github.io is alive, Registry Expiry Date: NOT FOUND
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,55 @@
-def load_urls4check(path):
-    pass
+import sys
+import requests
+import subprocess
+import os
 
-def is_server_respond_with_200(url):
-    pass
 
-def get_domain_expiration_date(domain_name):
-    pass
+MAX_RESPONSE_TIMEOUT = 7  # max response timeout to get answer from site
+user_agent = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                            "Chrome/59.0.3071.104 Safari/537.36"}
+temp_file = "temp_domain_info.txt"
+
+
+def load_urls4check(path: "str file name") -> "list of urls":
+    with open(path, mode='r') as f:
+        lines = [name.strip() for name in f.readlines()]
+    return lines
+
+
+def is_server_respond_with_200(url: "str url") -> "bool":
+    try:
+        response = requests.get("http://"+url, headers=user_agent, timeout=MAX_RESPONSE_TIMEOUT)
+        if response.ok:  # response.status_code == 200
+            return True
+        else:
+            return False
+    except requests.exceptions.Timeout:
+        return False
+
+
+def get_domain_expiration_date(domain_name: "str") -> "list":
+    command_line = 'whois.exe -nobanner {} > {}'.format(domain_name, temp_file)
+    result = subprocess.run(command_line, shell=True)
+    if result.returncode == 0:   # no errors
+        with open(temp_file, mode='r') as f:
+            lines = [line.strip() for line in f.readlines()]
+        date_line = [line for line in lines if "Registry Expiry Date:" in line]
+        os.remove(temp_file)
+        return date_line
+
+
+def main(urls_list: "list"):
+    for url in urls_list:
+        if is_server_respond_with_200(url):
+            print("site {} is alive,".format(url), end=" ")
+        else:
+            print("site {} is dead ".format(url), end=" ")
+        expiry_date = get_domain_expiration_date(url)
+        if expiry_date:
+            print(expiry_date[0])
+        else:
+            print("Registry Expiry Date: NOT FOUND")
 
 if __name__ == '__main__':
-    pass
+    urls = load_urls4check(sys.argv[1:][0])
+    main(urls)
